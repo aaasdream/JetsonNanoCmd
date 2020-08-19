0387f00bed90f266ef3375a0963be35f2a827a12
aaasdream <60680961+aaasdream@users.noreply.github.com>
2020-08-19 14:37:32 +0800

    Update and rename 啟動VNC指令 to Install_VNC.sh

--- Install_VNC.sh
+++ Install_VNC.sh
@@ -1,7 +1,5 @@
-
+#!/bin/bash
 # 啟動VNC的指令紀錄
-
-```
 sudo apt update
 sudo apt install vino
 sudo ln -s ../vino-server.service     /usr/lib/systemd/user/graphical-session.target.wants
@@ -10,4 +8,3 @@ gsettings set org.gnome.Vino require-encryption false
 gsettings set org.gnome.Vino authentication-methods "['vnc']"
 gsettings set org.gnome.Vino vnc-password $(echo -n '請修改密碼'|base64)
 sudo reboot
-```
