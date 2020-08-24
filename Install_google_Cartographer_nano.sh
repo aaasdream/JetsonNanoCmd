74208b6c074e1c2874e50ebe8962df5e74c731db
aaasdream <60680961+aaasdream@users.noreply.github.com>
2020-08-24 10:44:15 +0800

    Update Install_google_Cartographer_nano.sh

--- Install_google_Cartographer_nano.sh
+++ Install_google_Cartographer_nano.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
-#PC版本與嵌入式裝置是不相同的 ，PC版本如下網址
-https://google-cartographer.readthedocs.io/en/latest/ 看一下這裡
+#這是安裝Nvidia Jetson nano版本的Cartographer
+# ros : melodic
+# 安裝路徑 ~/catkin_google_ws
+
+
+#jetson nano 與PC版安裝差異
+#   增加安裝ros-melodic-pcl-conversions   ros-melodic-tf2-eigen
+# 注意事項 請在安裝openvslam之前安裝，
+# 執行即可 不需要任何參數不需要權限，安裝過程會詢問最高權限密碼
+
+# 官方安裝教學網址
 #  https://google-cartographer-ros.readthedocs.io/en/latest/compilation.html#building-installation
-# 嵌入式版本 如下網址
-#https://google-cartographer-ros-for-turtlebots.readthedocs.io/en/latest/
-# 
-# 本文件使用嵌入式版本指令安裝，給jetson nano使用
 
-# Install wstool and rosdep.
+
+
 sudo apt-get update
+sudo apt-get install -y ros-melodic-pcl-conversions ros-melodic-tf2-eigen
 sudo apt-get install -y python-wstool python-rosdep ninja-build
-
-# Create a new workspace in 'catkin_ws'.
-mkdir catkin_googlews
-cd catkin_googlews
+cd ~
+mkdir catkin_google_ws
+cd catkin_google_ws
 wstool init src
-
-# Merge the cartographer_turtlebot.rosinstall file and fetch code for dependencies.
-wstool merge -t src https://raw.githubusercontent.com/googlecartographer/cartographer_turtlebot/master/cartographer_turtlebot.rosinstall
+wstool merge -t src https://raw.githubusercontent.com/cartographer-project/cartographer_ros/master/cartographer_ros.rosinstall
 wstool update -t src
 
-# Install deb dependencies.
-# The command 'sudo rosdep init' will print an error if you have already
-# executed it since installing ROS. This error can be ignored.
+src/cartographer/scripts/install_proto3.sh
 sudo rosdep init
 rosdep update
-rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y
-
-# Build and install.
+rosdep install --from-paths src --ignore-src --rosdistr==${ROS_DISTRO} -y -i -r
 catkin_make_isolated --install --use-ninja
+
 source install_isolated/setup.bash
-echo "source ~/catkin_googlews/install_isolated/setup.bash" >> ~/.bashrc
-
-
+echo "source ~/catkin_google_ws/install_isolated/setup.bash" >> ~/.bashrc
 
 echo "Download and run demo."
 wget -P ~/Downloads https://storage.googleapis.com/cartographer-public-data/bags/backpack_2d/cartographer_paper_deutsches_museum.bag
 roslaunch cartographer_ros demo_backpack_2d.launch bag_filename:=${HOME}/Downloads/cartographer_paper_deutsches_museum.bag
+
 
 
