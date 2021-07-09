ddaa26cb18cd577696bf49dc34e03fe4ab06e9dc
aking <aaasdream@gmail.com>
2021-07-09 13:51:54 +0800

    jetson inference

---
+++ Install_jetson-inference.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install git cmake libpython3-dev python3-numpy
+git clone --recursive https://github.com/dusty-nv/jetson-inference
+cd jetson-inference
+mkdir build
+cd build
+cmake ../
+make -j$(nproc)
+sudo make install
+sudo ldconfig
