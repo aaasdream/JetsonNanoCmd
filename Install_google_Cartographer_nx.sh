0101a3aa5950ad7544033cfa85086c520348c4c1
aking <aaasdream@gmail.com>
2021-09-08 15:37:47 +0800

    cartographer

--- Install_google_Cartographer_nx.sh
+++ Install_google_Cartographer_nx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#這是安裝Nvidia Jetson nano版本的Cartographer
+#這是安裝Nvidia Jetson naon and nx版本的Cartographer 
 # ros : melodic
 # 安裝路徑 ~/catkin_google_ws
 
@@ -16,20 +16,50 @@
 
 sudo apt-get update
 sudo apt-get install -y ros-melodic-pcl-conversions ros-melodic-tf2-eigen
-sudo apt-get install -y python-wstool python-rosdep ninja-build
+sudo apt-get install -y python3-wstool python3-rosdep ninja-build stow
+sudo apt-get install -y stow
+sudo apt-get install -y ninja-build
+sudo apt-get install -y liblua5.2-dev
+
 cd ~
 mkdir catkin_google_ws
 cd catkin_google_ws
+#install ceres-solver
+wget http://ceres-solver.org/ceres-solver-2.0.0.tar.gz
+# CMake
+sudo apt-get install -y cmake
+# google-glog + gflags
+sudo apt-get install -y libgoogle-glog-dev libgflags-dev
+# BLAS & LAPACK
+sudo apt-get install -y libatlas-base-dev
+# Eigen3
+sudo apt-get install -y libeigen3-dev
+# SuiteSparse and CXSparse (optional)
+sudo apt-get install -y libsuitesparse-dev
+
+tar zxf ceres-solver-2.0.0.tar.gz
+mkdir ceres-bin
+cd ceres-bin
+cmake ../ceres-solver-2.0.0
+make -j3
+sudo make install
+
+
+
+cd ~/catkin_google_ws
+
+
 wstool init src
 wstool merge -t src https://raw.githubusercontent.com/cartographer-project/cartographer_ros/master/cartographer_ros.rosinstall
 wstool update -t src
 
-src/cartographer/scripts/install_proto3.sh
 sudo rosdep init
 rosdep update
-source /opt/ros/melodic/setup.bash
-rosdep install --from-paths src --ignore-src --rosdistr==${ROS_DISTRO} -y -i -r
+rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y
+
+src/cartographer/scripts/install_abseil.sh
 catkin_make_isolated --install --use-ninja
+
 
 source install_isolated/setup.bash
 echo "source ~/catkin_google_ws/install_isolated/setup.bash" >> ~/.bashrc
