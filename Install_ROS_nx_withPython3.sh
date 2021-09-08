aeb0123fc897f6aca8c5a2a773aaa97aec903217
aking <aaasdream@gmail.com>
2021-09-08 11:10:40 +0800

    ros py3

--- Install_ROS_nx_withPython3.sh
+++ Install_ROS_nx_withPython3.sh
@@ -5,9 +5,15 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
 sudo apt update
+sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool  build-essential
 sudo apt install -y ros-melodic-desktop-full
 sudo apt install -y python3-pip python3-all-dev python3-rospkg
 sudo apt install -y ros-melodic-desktop-full --fix-missing
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+source /opt/ros/melodic/setup.bash
+sudo rosdep init
+rosdep update
 
 
 
